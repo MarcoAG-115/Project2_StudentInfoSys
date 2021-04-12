ddb93acf23497d51dd9510bf0637ca7bc0511310
MarcoAG-115 <mgprime115@gmail.com>
2021-04-12 17:02:00 -0500

    First commit

---
+++ src/LoginGUI.java
@@ -0,0 +1,77 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginGUI implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
+    public static void main(String[] args) {
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+        
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new LoginGUI());
+        panel.add(button);
+        
+        success = new JLabel("");
+        success.setBounds(10, 110, 350, 25);
+        panel.add(success);
+
+        frame.setVisible(true);
+
+        //JPanel panel = new JPanel();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+       String user = userText.getText();
+       String password = passwordText.getText();
+       //System.out.println(user + ", " + password);
+
+       if(user.equals("studentID") && password.equals("studentPass123")){
+            success.setText("Login successful. Accessing Student View.");
+       }
+       else if(user.equals("instructorID") && password.equals("adminPass123")){
+            success.setText("Login successful. Accessing Administrator View.");
+       }
+       else{
+            success.setText("Login Failed. Incorrect Username and Password.");
+       }
+        
+    }
+}
